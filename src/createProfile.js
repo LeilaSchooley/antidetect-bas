6366a46749f61140a312e10b1be7d7cfc7c855b3
Isaac Boadi <46610308+LeilaSchooley@users.noreply.github.com>
2024-06-03 14:58:35 +0100

    refactor

--- src/createProfile.js
+++ src/createProfile.js
@@ -11,15 +11,36 @@ function createProfile(
   };
 
   insertAccountRow(accountData);
-  removeTable();
+  //removeTable();
   //createTableHeader(columnNames.account);
+}
+async function insertAccountRow(data) {
+  // Create an object representing the account data
 
-  loadPageData(
-    1,
-    accountTableId,
-    accountColumns,
-    accountTableId,
-    taskTableId,
-    logId
-  ); // Load the first page of account data when the page loads
+  // Get the table id of the Accounts table
+  var tableId = Api.GetDatabaseStructure().find(function (table) {
+    return table.name == "browsers";
+  }).id;
+
+  // Get the columns for the Accounts table
+  var columns = Api.GetDatabaseStructure().find(function (table) {
+    return table.name == "browsers";
+  }).columns;
+
+  // Create an object to hold the data for the new row
+  var row = {};
+
+  // Populate the object with the data for the new row
+
+  row[columns.find((column) => column.name === "proxy").id] = data["proxy"];
+  row[columns.find((column) => column.name === "name").id] = data["name"];
+
+  // Insert the new row into the table
+  Api.DatabaseInsert([], row, tableId)
+    .then((InsertedRecordId) => {
+      console.log(`browser Row inserted successfully ${InsertedRecordId}`);
+    })
+    .catch((error) => {
+      console.log("Error inserting row:", error);
+    });
 }
